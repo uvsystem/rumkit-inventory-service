f0e56e4a57c8df8404702023e67251552cf1a6e7
Deddy Kakunsi <deddykakunsi@outlook.com>
2015-10-12 15:50:19 +0800

    Online database.

--- src/com/dbsys/rs/inventory/DbConnectionConfig.java
+++ src/com/dbsys/rs/inventory/DbConnectionConfig.java
@@ -1,8 +1,7 @@
 package com.dbsys.rs.inventory;
 
 public class DbConnectionConfig {
-	// public static final String HOST = "mysql31535-core-unitedvision.whelastic.net";
-	public static final String HOST = "localhost";
+	public static final String HOST = "mysql31535-core-unitedvision.whelastic.net";
 	public static final String PROPERTY_NAME_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
     public static final String PROPERTY_NAME_DATABASE_URL = String.format("jdbc:mysql://%s:3306/rumkit", HOST);
     public static final String PROPERTY_NAME_DATABASE_USERNAME = "rumkit_user";
