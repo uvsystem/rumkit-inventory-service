cc2606770e39c6382d7e194b14b6363759882030
Deddy Kakunsi <deddykakunsi@outlook.com>
2015-10-17 12:24:49 +0800

    Update versi security.

--- src/com/dbsys/rs/inventory/DbConnectionConfig.java
+++ src/com/dbsys/rs/inventory/DbConnectionConfig.java
@@ -1,7 +1,8 @@
 package com.dbsys.rs.inventory;
 
 public class DbConnectionConfig {
-	public static final String HOST = "mysql31535-core-unitedvision.whelastic.net";
+//	public static final String HOST = "mysql31535-core-unitedvision.whelastic.net";
+	public static final String HOST = "localhost";
 	public static final String PROPERTY_NAME_DATABASE_DRIVER = "com.mysql.jdbc.Driver";
     public static final String PROPERTY_NAME_DATABASE_URL = String.format("jdbc:mysql://%s:3306/rumkit", HOST);
     public static final String PROPERTY_NAME_DATABASE_USERNAME = "rumkit_user";
